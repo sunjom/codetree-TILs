f35e9e6cc95e14816b81a72e4381398414371dcb
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-09-27 17:54:08 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240927/연속부분수열일까/contiguous-array-or-not.py
+++ 240927/연속부분수열일까/contiguous-array-or-not.py
@@ -12,12 +12,12 @@ else:
 if(arr.count(num) >= 1):
     check = arr.index(num);
 
-    Bool = False;
-    for i in range(check, len(arr)-1):
-        if(arr[i:i+b] == brr):
-            Bool = True;
-            break
-    if(Bool):
-        print("Yes");
-    else:
-        print("No");
+Bool = False;
+for i in range(check, len(arr)-1):
+    if(arr[i:i+b] == brr):
+        Bool = True;
+        break
+if(Bool):
+    print("Yes");
+else:
+    print("No");
