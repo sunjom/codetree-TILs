e7c56f353fdad1009064a3dd81179bb9bc262e07
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-09-28 12:42:01 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 196ms, Memory: 25MB, Status: Passed - CodeTree

--- 240928/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240928/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -3,11 +3,11 @@ arr = [0] +list(map(int,input().split()));
 MVal = arr.index(max(arr));
 idx = [MVal]
 while True:
+    if(MVal == 1):
+        break;
     Marr = arr[:MVal]
     MVal = arr.index(max(Marr));
     idx.append(MVal);
-    if(MVal == 1):
-        break;
     
 for i in idx:
     print(i , end=" ")
