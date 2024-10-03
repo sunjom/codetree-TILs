b17476eb14014187f02a15b49c9f12e6c1732d4a
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-10-03 12:04:50 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 213ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241003/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,9 @@
+n = int(input());
+def Return(n):
+    if(n == 1):
+        return 0
+    if(n%2 == 0):
+        return 1 + Return(n//2);
+    else:
+        return 1 + Return(n*3 +1)
+print(Return(n))
