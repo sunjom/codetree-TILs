7f751c5c65cb8b6e5e6f8faed7874e7b91759fee
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-10-04 14:53:32 +0900

    Title: 요일 맞추기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/요일 맞추기/guess-day-of-week.py
+++ 241004/요일 맞추기/guess-day-of-week.py
@@ -1,9 +1,17 @@
 a,b,c,d = list(map(int,input().split()));
 
-Date = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+def ReturnDay(n):
+    Date = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    Sum = 0
+    for i in range(1,n):
+        Sum += Date[i];
+    return Sum;
+
 State = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-cDate = 0
-for i in range(1,c):
-    cDate += Date[i];
-cDate += d
-print(State[cDate%7])
+
+diff = (ReturnDay(a) + b ) - (ReturnDay(c) - d);
+
+while diff < 0:
+    diff += 7;
+
+print(State[diff%7])
