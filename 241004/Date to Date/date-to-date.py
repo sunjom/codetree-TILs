321d5cded7c86fcdcd8140e6220b9e36faf34250
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-10-04 14:32:08 +0900

    Title: Date to Date, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/Date to Date/date-to-date.py
+++ 241004/Date to Date/date-to-date.py
@@ -3,8 +3,10 @@ a,b,c,d = list(map(int,input().split()));
 Day = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 aDay = 0
 cDay = 0
-for i in range(1,a+1):
+for i in range(1,a):
     aDay += Day[i];
-for i in range(1,c+1):
+for i in range(1,c):
     cDay += Day[i];
-print(cDay-aDay+d-b);
+print(d)
+print(b)
+print(cDay-aDay+d-b+1);
