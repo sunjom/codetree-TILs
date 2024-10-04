cdf7332c2e88a8bfdf40313ce884ac2445bea71a
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-10-04 14:57:24 +0900

    Title: 요일 맞추기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/요일 맞추기/guess-day-of-week.py
+++ 241004/요일 맞추기/guess-day-of-week.py
@@ -7,9 +7,23 @@ def ReturnDay(n):
         Sum += Date[i];
     return Sum;
 
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for i in range(1, m):
+        total_days += days[i]
+    
+    # m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d
+    
+    return total_days
+
 State = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-diff = (ReturnDay(a) + b ) - (ReturnDay(c) + d);
+diff = (num_of_days(c,d) ) - (num_of_days(a,b));
 
 while diff < 0:
     diff += 7;
