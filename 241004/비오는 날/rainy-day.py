50730691d234d741d139bb267465fd9ed97644ce
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2024-10-04 12:44:20 +0900

    Title: 비오는 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241004/비오는 날/rainy-day.py
@@ -0,0 +1,21 @@
+n = int(input());
+Arr = []
+class RainDay:
+    def __init__(self, day, week, state):
+        self.day =day;
+        self.week =week;
+        self.state = state;
+for i in range(n):
+    day,week,state = tuple(input().split());
+    Arr.append(RainDay(day,week,state));
+
+LatestRain = -1
+
+for i in range(n):
+    if(Arr[i].state == 'Rain'):
+        if(LatestRain == -1):
+            LatestRain = i;
+            continue;
+        elif(Arr[LatestRain].day > Arr[i].day):
+            LatestRain = n;
+print(Arr[LatestRain].day,Arr[LatestRain].week,Arr[LatestRain].state)
