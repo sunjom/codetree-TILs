d246481243e55ce9d6671e5a1750a5ce6da8709f
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-03-26 11:55:30 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250326/Date to Date/date-to-date.py
+++ 250326/Date to Date/date-to-date.py
@@ -1,9 +1,9 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 # Please write your code here.
-date = [31,28,31,30,31,30,31,31,30,31,30,31]
+date = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 sumA = sum(date[i] for i in range(m1)) + d1
 sumB = sum(date[i] for i in range(m2)) + d2
 
-print(sumB - sumA)
+print(sumB - sumA+1)
