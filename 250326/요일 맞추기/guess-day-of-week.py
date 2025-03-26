5037880cf772b0434b7c18303f321fbcfbf023a5
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-03-26 12:22:10 +0900

    Title: 요일 맞추기, Time: 88ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250326/요일 맞추기/guess-day-of-week.py
+++ 250326/요일 맞추기/guess-day-of-week.py
@@ -9,10 +9,7 @@ hapA = sum(days[i] for i in range(m1)) + d1
 hapB = sum(days[i] for i in range(m2)) + d2
 
 num = hapB-hapA
-if(num < 0):
-    nums = abs(num) % 7
-    nums *= -1
-    print(nums)
-    print(week[nums])
-else:
-    print(week[num%7])
+while num < 0:
+    num += 7
+
+print(week[num])
