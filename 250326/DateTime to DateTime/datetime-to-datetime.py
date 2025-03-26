f1cef32255f14bfc1c208795f4dc0271061a69ad
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-03-26 12:03:27 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250326/DateTime to DateTime/datetime-to-datetime.py
+++ 250326/DateTime to DateTime/datetime-to-datetime.py
@@ -2,4 +2,8 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 
-print(((a-11) * 24*60) + (b-11)*60 + (c-11)) 
+answer= ((a-11) * 24*60) + (b-11)*60 + (c-11)
+if(answer < 0):
+    print(-1)
+else:
+    print(answer)
