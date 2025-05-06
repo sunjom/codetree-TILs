d2b211b92d983f86e84b87e3c64c918763ef1ab6
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-05-06 22:57:20 +0900

    Title: 2진수로 변환하기, Time: 610ms, Memory: 160MB, Status: Memory Limit Exceed - Codetree

---
+++ 250506/2진수로 변환하기/convert-to-binary.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+const answer = []
+let cur = n
+
+while (cur !== 1) {
+  answer.push(cur % 2)
+  cur = Math.floor(cur / 2)
+}
+
+answer.push(1)
+
+console.log(answer.reverse().join(''))
