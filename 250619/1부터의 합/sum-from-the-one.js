04654ca241851e108451fe21f87de5f23cb0bd36
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-06-19 13:54:35 +0900

    Title: 1부터의 합, Time: 194ms, Memory: 20MB, Status: Passed - Codetree

--- 250619/1부터의 합/sum-from-the-one.js
+++ 250619/1부터의 합/sum-from-the-one.js
@@ -3,9 +3,9 @@ const input = fs.readFileSync(0).toString().trim();
 
 const N = Number(input);
 
-let result = 1;
+let result = 0;
 
-for(let i = 2 ; i < 101 ; i++){
+for(let i = 1 ; i < 101 ; i++){
     result += i;
     if(result >= N){
         console.log(i);
