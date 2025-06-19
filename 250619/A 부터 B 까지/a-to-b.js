76515e627b4ca95ad083926e89a898b6b03285da
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-06-19 14:47:58 +0900

    Title: A 부터 B 까지, Time: 173ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250619/A 부터 B 까지/a-to-b.js
+++ 250619/A 부터 B 까지/a-to-b.js
@@ -4,6 +4,7 @@ const inputs = fs.readFileSync(0).toString().trim().split(' ');
 let A = Number(inputs[0]);
 const B = Number(inputs[1]);
 
+let result = ''
 while(A < B){
     result += (A +' ');
     if(A % 2 == 1){
