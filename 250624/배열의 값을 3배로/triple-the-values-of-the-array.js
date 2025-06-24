16434d450fbf4f9c5917c1c38a0efe27ad0c07ed
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-06-24 11:24:11 +0900

    Title: 배열의 값을 3배로, Time: 211ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250624/배열의 값을 3배로/triple-the-values-of-the-array.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const inputs = fs.readFileSync(0).toString().trim().split('\n');
+
+const arr = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+for(let i = 0 ; i < inputs.length ; i++){
+    arr[i] = inputs[i].split(" ").map(Number);
+}
+
+for(let i = 0 ; i < inputs.length ; i++){
+    for(let e = 0 ; e <inputs[i].length ; e++){
+        arr[i][e] = *= 3;
+    }
+}
+
+for(let i = 0 ; i < 3 ; i++){
+    let str = "";
+    for(let j = 0 ; j < 3 ; j++){
+        str+= arr[i][j] + " ";
+    }
+    console.log(str);
+}
