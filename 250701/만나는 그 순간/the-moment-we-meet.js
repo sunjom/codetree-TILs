f34201ba0465ced7b4330ead1a01c9bd1ca51451
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-07-01 13:41:22 +0900

    Title: 만나는 그 순간, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250701/만나는 그 순간/the-moment-we-meet.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m] = input[0].split(' ').map(Number);
+let line = 1;
+const movesA = [];
+for (let i = 0; i < n; i++) {
+    const [d, t] = input[line++].split(' ');
+    movesA.push([d, Number(t)]);
+}
+const movesB = [];
+for (let i = 0; i < m; i++) {
+    const [d, t] = input[line++].split(' ');
+    movesB.push([d, Number(t)]);
+}
+
+// Please Write your code here.
+
+const A = [0];
+const B = [0];
+
+let AIdx = 0;
+let BIdx = 0;
+for(let i of movesA){
+    let sum = A[AIdx];
+    const [move,distance] = i;
+    if(move == 'R'){
+        for(let e = 0 ; e < distance ; e++){
+            A.push(++sum);
+            AIdx++;
+        }
+    }else{
+        for(let e = 0 ; e < distance ; e++){
+            A.push(--sum);
+            AIdx++;
+        }
+    }
+}
+
+for(let i of movesB){
+    let sum = B[BIdx];
+    const [move,distance] = i;
+    if(move == 'R'){
+        for(let e = 0 ; e < distance ; e++){
+            B.push(++sum);
+            BIdx++;
+        }
+    }else{
+        for(let e = 0 ; e < distance ; e++){
+            B.push(--sum);
+            BIdx++;
+        }
+    }
+}
+
+for(let i = 1 ; i < A.length ; i++){
+    if(A[i] == B[i]){
+        console.log(i);
+        break;
+    }
+}
