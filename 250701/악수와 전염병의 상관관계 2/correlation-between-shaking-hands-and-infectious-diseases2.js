28e16439f048df1013e566f51f79a36c76517b90
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-07-01 14:49:28 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 183ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250701/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
+++ 250701/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -2,34 +2,36 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k, p, t] = input[0].split(' ').map(Number);
 const shakes = [];
-
 for (let i = 1; i <= t; i++) {
     const [time, person1, person2] = input[i].split(' ').map(Number);
     shakes.push({ time, person1, person2 });
 }
+// Please write your code here.
+//n => 개발자 수
+//k => 감염된 사람이 감염시킬 수 있는 만남의 수
+//p => 최조 감염자
+//t => 시행 횟수
 
 const Person = Array.from({ length: n + 1 }, () => ({ inf: 0, meet: 0 }));
-Person[p].inf = 1;
-
-shakes.sort((a, b) => a.time - b.time);
-
-for (let { person1, person2 } of shakes) {
-    const p1 = Person[person1];
-    const p2 = Person[person2];
-
-    if (p1.inf === 1 && p1.meet < k) {
-        if (p2.inf === 0) p2.inf = 1;
-        p1.meet++;
+Person[p] = {inf:1,meet:0};
+shakes.sort((prev,cur) => prev.time - cur.time);
+for(let meets of shakes){
+    const {person1,person2} = meets; 
+    
+    if((Person[person1].inf == 1 && Person[person1].meet < k)){
+        Person[person2].inf = 1;
+        Person[person2].meet--;
+    }else if((Person[person2].inf == 1 && Person[person2].meet < k)){
+        Person[person1].inf = 1;
+        Person[person1].meet--;
     }
-
-    if (p2.inf === 1 && p2.meet < k) {
-        if (p1.inf === 0) p1.inf = 1;
-        p2.meet++;
-    }
+    Person[person1].meet++;
+    Person[person2].meet++;
 }
 
-let result = '';
-for (let i = 1; i <= n; i++) {
-    result += Person[i].inf;
+let str = '';
+for(let i = 1 ; i < Person.length; i++){
+    str += Person[i].inf+'';
 }
-console.log(result);
+
+console.log(str);
