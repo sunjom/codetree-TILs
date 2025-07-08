7944fca536fd84941224fffb59dc8baef8c25088
You_seon_jong <102607647+sunjom@users.noreply.github.com>
2025-07-08 12:31:21 +0900

    Title: 작은 구슬의 이동, Time: 176ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250708/작은 구슬의 이동/small-marble-movement.js
+++ 250708/작은 구슬의 이동/small-marble-movement.js
@@ -3,33 +3,39 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, t] = input[0].split(" ").map(Number);
 let [r, c, d] = input[1].split(" ");
-r = Number(r);
-c = Number(c);
+r = Number(r) - 1;
+c = Number(c) - 1;
 
 // Please Write your code here.
 // N => 행렬, T => 시간,  r,c, => 초기 위치, D => 이동방향
-
-const xList = [0,1,0,-1];
-const yList = [1,0,-1,0];
+1 , 3 
+const xList = [0,1,-1,0];
+const yList = [1,0,0,-1];
 const dir = {
     'U':3,
     'D':1,
     'R':0,
     'L':2,
 }
+
+
 let idx = dir[d];
-let time = 0;
-while(true){
-    if(time >= t) break;
-    r += xList[idx]; c += yList[idx];
-    if((d =='L' || d == 'R') && (c == 1 || c == n)){
-        time +=1;
-        idx = idx >= 2 ? 2 - idx : 2 + idx;
-    }else if((d =='U' || d == 'D') && (r == 1 || r == n)){
-        time +=1;
-        idx = idx >= 2 ? 2 - idx : 2 + idx;
-    }
-    time += 1;
+
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-console.log(r,c);
+for(let i = 0 ; i < t ; i++){
+    const nr = r + xList[idx];
+    const nc = c + yList[idx];
+
+    if(inRange(nr,nc)){
+        r = nr;
+        c = nc;
+    }else{
+        idx = 3 - idx;
+    }
+}
+
+
+console.log(r + 1,c + 1);
